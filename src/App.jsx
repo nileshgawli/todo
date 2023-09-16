fb2fa98259effbe930618bbacb58d3e0a274dd93
Nilesh Gawli <nbgawli@gmail.com>
2023-09-16 18:05:19 +0530

    feat: add the api integration to get all tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Timeline from "./components/Timeline";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import axios from 'axios';
 
 function App() {
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://todo-555sakfi8-kolwankar-siddhiraj.vercel.app/api/task/get/all?page=1")
+    .then((res)=> {
+      setTasks(res.data.data);
+    })
+    .catch((err)=> {
+      console.log(err);
+    })
+  }, []);
+
   return (
     <>
       <div className="h-screen bg-accent">
@@ -15,11 +29,7 @@ function App() {
           <div className="w-full flex justify-center">
             <div className="w-[80%]">
               <AddTask />
-              <Tasks/>
-              <Tasks/>
-              <Tasks/>
-              <Tasks/>
-              <Tasks/>
+              <Tasks tasks = {tasks}/>
             </div>
           </div>
         </div>
